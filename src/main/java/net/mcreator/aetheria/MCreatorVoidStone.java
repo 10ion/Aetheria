a927e4a991ab89fb1b0491ef58634c047e8101b1
10ionic <10ioniumic@gmail.com>
2020-02-20 18:37:24 -0700

    added voidstone

---
+++ src/main/java/net/mcreator/aetheria/MCreatorVoidStone.java
@@ -0,0 +1,48 @@
+package net.mcreator.aetheria;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+@Elementsaetheria.ModElement.Tag
+public class MCreatorVoidStone extends Elementsaetheria.ModElement {
+	@ObjectHolder("aetheria:voidstone")
+	public static final Block block = null;
+
+	public MCreatorVoidStone(Elementsaetheria instance) {
+		super(instance, 137);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("voidstone");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
