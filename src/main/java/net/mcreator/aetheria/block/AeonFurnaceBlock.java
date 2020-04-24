4531f7a22df98ab9bc13252163dea8f6b89cfb9f
AgentLoneStar007 <lonestarvids07@gmail.com>
2020-04-23 19:16:39 -0500

    Added smelting recipes to all ores.

---
+++ src/main/java/net/mcreator/aetheria/block/AeonFurnaceBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.aetheria.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.aetheria.itemgroup.AetheriaCraftingItemGroup;
+import net.mcreator.aetheria.AetheriaElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AetheriaElements.ModElement.Tag
+public class AeonFurnaceBlock extends AetheriaElements.ModElement {
+	@ObjectHolder("aetheria:aeonfurnace")
+	public static final Block block = null;
+	public AeonFurnaceBlock(AetheriaElements instance) {
+		super(instance, 407);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(AetheriaCraftingItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("aeonfurnace");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
